cbb5bbfb4d0a9b175ab4db8af5da28b00f06f98b
Darius Mclean <126315489+NotDurris@users.noreply.github.com>
2023-05-10 14:21:45 +1000

    FixedBoxGlitchyness

--- My project/Assets/Scripts/GrowingShrinking/FillingObject.cs
+++ My project/Assets/Scripts/GrowingShrinking/FillingObject.cs
@@ -17,6 +17,7 @@ public class FillingObject : MonoBehaviour, IGrowable, IPickUp
     }
 
     private Rigidbody rb;
+    private float boundary = 3.13f;
 
     void Start(){
         rb = GetComponent<Rigidbody>();
@@ -28,42 +29,53 @@ public class FillingObject : MonoBehaviour, IGrowable, IPickUp
     void Update(){
         rb.mass = scaleFactor.x + scaleFactor.y + scaleFactor.z / 3;
         transform.localScale = scaleFactor;
-    }
 
-    public void Grow(){       
-        float yGrowth = Mathf.Clamp(transform.localScale.y, 1.0f, 5.0f);
         if(Physics.BoxCast(transform.position, 
                            new Vector3(transform.localScale.x/2, transform.localScale.y/5,transform.localScale.z/2), 
                            transform.up, transform.rotation,
-                           scaleFactor.y/3.2f) &&
+                           scaleFactor.y / boundary) &&
             Physics.BoxCast(transform.position, 
                            new Vector3(transform.localScale.x/2, transform.localScale.y/5,transform.localScale.z/2), 
                            -transform.up, transform.rotation,
-                           scaleFactor.y/3.2f)){
+                           scaleFactor.y / boundary)){
+            Debug.Log("Blocked");
+        }
+    }
+
+    public void Grow(){       
+        float yGrowth = Mathf.Clamp(transform.localScale.y, 0.1f, 5.0f);
+        if(Physics.BoxCast(transform.position, 
+                           new Vector3(transform.localScale.x/2, transform.localScale.y/5,transform.localScale.z/2), 
+                           transform.up, transform.rotation,
+                           scaleFactor.y / boundary) &&
+            Physics.BoxCast(transform.position, 
+                           new Vector3(transform.localScale.x/2, transform.localScale.y/5,transform.localScale.z/2), 
+                           -transform.up, transform.rotation,
+                           scaleFactor.y / boundary)){
             yGrowth = 0;
         }
 
-        float xGrowth = Mathf.Clamp(transform.localScale.x, 1.0f, 5.0f);
+        float xGrowth = Mathf.Clamp(transform.localScale.x, 0.1f, 5.0f);
         if(Physics.BoxCast(transform.position, 
                            new Vector3(transform.localScale.x/5, transform.localScale.y/2,transform.localScale.z/2), 
                            transform.right, transform.rotation,
-                           scaleFactor.x/3.2f) && 
+                           scaleFactor.x / boundary) && 
             Physics.BoxCast(transform.position, 
                            new Vector3(transform.localScale.x/5, transform.localScale.y/2,transform.localScale.z/2), 
                            -transform.right, transform.rotation,
-                           scaleFactor.x/3.2f)){
+                           scaleFactor.x / boundary)){
             xGrowth = 0;
         }
 
-        float zGrowth = Mathf.Clamp(transform.localScale.z, 1.0f, 5.0f);
+        float zGrowth = Mathf.Clamp(transform.localScale.z, 0.1f, 5.0f);
         if(Physics.BoxCast(transform.position, 
                            new Vector3(transform.localScale.x/2, transform.localScale.y/2,transform.localScale.z/5), 
                            transform.forward, transform.rotation,
-                           scaleFactor.z/3.2f) && 
+                           scaleFactor.z / boundary) && 
             Physics.BoxCast(transform.position, 
                            new Vector3(transform.localScale.x/2, transform.localScale.y/2,transform.localScale.z/5), 
                            -transform.forward, transform.rotation,
-                           scaleFactor.z/3.2f)){
+                           scaleFactor.z / boundary)){
             zGrowth = 0;
         }
 

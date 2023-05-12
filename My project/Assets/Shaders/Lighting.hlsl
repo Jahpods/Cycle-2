39e520b5b56565e9e217bb3460a633e036eecd58
Darius Mclean <126315489+NotDurris@users.noreply.github.com>
2023-05-12 21:25:53 +1000

    lightingOverhaul

--- My project/Assets/Shaders/Lighting.hlsl
+++ My project/Assets/Shaders/Lighting.hlsl
@@ -20,7 +20,7 @@
 
 void CalculateMainLight_float(float3 WorldPos, out float3 Direction, out float3 Color, out half DistanceAtten, out half ShadowAtten) {
 #ifdef SHADERGRAPH_PREVIEW
-    Direction = half3(0.5, 0.5, 0);
+    Direction = half3(0, 0, 0);
     Color = 1;
     DistanceAtten = 1;
     ShadowAtten = 1;
@@ -39,27 +39,29 @@ void CalculateMainLight_float(float3 WorldPos, out float3 Direction, out float3 
 #endif
 }
 
-void AddAdditionalLights_float(float Smoothness, float3 WorldPosition, float3 WorldNormal, float3 WorldView,
-    float MainDiffuse, float MainSpecular, float3 MainColor,
-    out float Diffuse, out float Specular, out float3 Color) {
+float GetLightIntensity(float3 color){
+    return max(color.r, max(color.g, color.b));
+}
+
+void AddAdditionalLights_float(float3 WorldPosition, float3 WorldNormal,
+    float MainDiffuse, float3 mainColor,
+    out float Diffuse, out float3 Color, out float3 LightMap) {
     Diffuse = MainDiffuse;
-    Specular = MainSpecular;
-    Color = MainColor * (MainDiffuse + MainSpecular);
+    Color = mainColor;
+    LightMap = float3(0,0,0);
 
 #ifndef SHADERGRAPH_PREVIEW
     int pixelLightCount = GetAdditionalLightsCount();
     for (int i = 0; i < pixelLightCount; ++i) {
         Light light = GetAdditionalLight(i, WorldPosition);
         half NdotL = saturate(dot(WorldNormal, light.direction));
-        half atten = light.distanceAttenuation * light.shadowAttenuation;
+        half atten = light.distanceAttenuation * light.shadowAttenuation * GetLightIntensity(light.color);
         half thisDiffuse = atten * NdotL;
-        Diffuse = thisDiffuse;
+        Diffuse += thisDiffuse;
         Color = light.color * thisDiffuse;
+        LightMap = thisDiffuse > 0 ? float3(1,1,1) : float3(0,0,0);        
     }
 #endif
-    half total = Diffuse * Color;
-    // If no light touches this pixel, set the color to the main light's color
-    //Color = total <= 0 ? MainColor : Color / total;
 }
 
 #endif
